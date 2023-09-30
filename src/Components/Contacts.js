69704ed6b36fe84b8ce605e132d2997ba4f7405b
Rajkumar <114281193+rajkumarraghunathan@users.noreply.github.com>
2023-09-30 07:32:22 +0530

    Latest commit

--- src/Components/Contacts.js
+++ src/Components/Contacts.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { GrLinkedin } from 'react-icons/gr';
 import { BsGithub } from 'react-icons/bs';
 import axios from 'axios';
